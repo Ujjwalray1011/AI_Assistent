f72c786708fc6e6a584808baf398cf7bef645a8f
Ujjwal Kumar Ray <ujjwalkumarray3333@gmail.com>
2026-02-17 14:58:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ from langchain_core.documents import Document
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_community.chat_message_histories import ChatMessageHistory
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain.chains import create_retrieval_chain, create_history_aware_retriever
+from langchain_classic.text_splitter import RecursiveCharacterTextSplitter
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
+from langchain_classic.chains import create_retrieval_chain, create_history_aware_retriever
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
 from datetime import datetime
@@ -606,3 +606,4 @@ st.markdown("""
         Built with ❤️ using Streamlit · LangChain · Groq · FAISS · Conversational RAG
     </div>
 """, unsafe_allow_html=True)
+
